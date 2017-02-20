a2d0eabf6819b723e0d518edce0825b634c3ca9c
arfost <chevalier.frugier@gmail.com>
2017-02-20 22:44:34 +0100

    first skeleton for room and dull game to support it, not fonctionnal

--- lobbyModel.js
+++ lobbyModel.js
@@ -2,67 +2,94 @@
 
 var defaultConfig = require("./lobbyConfig.json")
 
-var getNewLobby = function(game, config){
-  return new Lobby(game, config);
+var getNewLobby = function(room, config){
+  return new Lobby(room, config);
 }
 
 module.exports.getNewLobby = getNewLobby;
-
+/**
+ * Le Lobby est chargé de contenir la liste des /rooms 
+ * de la maintenir, et de l'exposer au serveur
+ * 
+ * properties : 
+ * * config : un objet json contenant les configs du lobbyConfig
+ *      -propForList list des properties a afficher venant de la room
+ * * clientRoomList : array de resumé de room + getter
+ * * roomList : une map clef/valeur de rooms
+ * * room : reference static contenant le constructeur des room a maintenir
+ *  
+ * 
+ */
 class Lobby{
 
-  constructor(config){
+/**
+ * cree un nouveau lobby,
+ * -room:
+ *      type de room a utiliser pour creer une nouvelle partie
+ * 
+ * -config:(optionnel)
+ *        les configs a utiliser pour le lobby
+ */
+  constructor(room, config){
       if(config == undefined){
           this.config = defaultConfig;
       }else{
           this.config = config;
       }
-      this.clientGameList = [];
-      this.gameList = {};
-    this.game = game;
+      this.clientRoomList = [];
+      this.roomList = {};
+    this.room = room;
   }
   
-  add(gameOptions){
+  /**
+   * ajoute une nouvelle room au lobby,
+   * et l'ajoute ainsi que son resumé aux listes du lobby
+   * -roomOptions:
+   *            options de la room a creer, fournis par le client
+   * *return : key de la room
+   * 
+   */
+  add(roomOptions){
       var key = this.getNewKey();
-		var game = this.game.getNewGame(gameOptions);
-    	
-      for(var prop in this.config.propToAdd){
-          game[prop] = this.config.propToAdd[prop];
-      }
+		var room = this.room.getNewRoom(roomOptions, this.config.propForList);
 
-      this.gameList[key] = game;
-      game.gameClientInfo = {};
+      this.roomList[key] = room;
+      room.roomClientInfo = {};
 
       for(var prop of this.config.propForList){
-          var result = game;
+          var result = room;
           for(var subPath of prop.split('.')){
               result = result[subPath];
           }
-          game.gameClientInfo[prop] = result;
+          room.roomClientInfo[prop] = result;
       }
-      game.gameClientInfo.key = key;
-      this.clientGameList.push(game.gameClientInfo);
+      room.roomClientInfo.key = key;
+      this.clientRoomList.push(room.roomClientInfo);
 
       return key;
   }
-
+/**
+ * ajouter un joueur a une room
+ * -key:    
+ *      clef de la room a rejoindre
+ * -player: 
+ *          player a ajouter dans la room
+ */
   join(key, player){
 
-      var game = this.gameList[key];
-      if(game.players.length < game.places){
-          game.players.push(player);
-          return true;
-      }else{
-          return false;
-      }
+      var room = this.roomList[key];
+      return room.addPlayer(player);
+  }
+/**
+ * 
+ */
+  getClientRoomList(){
+      console.log(this.clientRoomList);
+      return this.clientRoomList;
   }
 
-  getClientGameList(){
-      console.log(this.clientGameList);
-      return this.clientGameList;
-  }
-
-  getGameDetail(key){
-      return this.gameList[key];
+  getRoomDetail(key){
+      return this.roomList[key];
   }
 
   getNewKey(){
---
+++ roomModel.js
@@ -0,0 +1,125 @@
+'use strict'
+
+/****
+ * Une room 
+ * 
+ * 
+ * 
+ * 
+ */
+
+var getNewRoom = function(roomConfig, lobbyConfig){
+  return new Room(roomConfig, lobbyConfig);
+}
+
+module.exports.getNewRoom = getNewRoom;
+
+class Room{
+  
+  constructor(roomConfig, propForList){
+    this.places = 2;
+    this.name = roomConfig.name;
+    this.desc = roomConfig.desc;
+    this.maxPlayer = roomConfig.maxPlayer;
+    this.players = [];
+    this.statut = "waiting"
+    this.game = new Game({
+      maxPlayer : roomConfig.maxPlayer
+    });
+    this.propForList = propForList;
+  }
+
+  updateClientInfo(){
+    for(var prop of this.propForList){
+          var result = this;
+          for(var subPath of prop.split('.')){
+              result = result[subPath];
+          }
+          this.roomClientInfo[prop] = result;
+      }
+  }
+  
+  addPlayer(playerProfil){
+    if(this.statut == "waiting"){
+      this.players.push(playerProfil);
+      if(this.players.length == this.maxPlayer){
+        this.statut = "launched";
+      }
+      this.updateClientInfo();
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+  
+  getPlayerList(){
+    return this.players;
+  }
+  
+  removePlayer(playerProfil){
+    
+  }
+}
+
+class Game{
+  constructor(gameOptions){
+    this.maxPlayer = gameOptions.maxPlayer; 
+    this.played = 0;
+    this.play = [];
+    this.score = {
+      gamePlayed : 0,
+      players:{}
+    };
+    for(player of gameOptions.players){
+      this.score.players[player] = {
+        win:0,
+        score:0
+      }
+    }
+  }
+  play(player, play){
+    this.played++;
+    this.play.push({
+      player:player,
+      play:play
+    });
+    if(this.played == this.maxPlayer){
+      var winner = "";
+      var score = 0;
+      for(var play of this.play){
+        if(100 - play.play > score){
+          winner = play.player;
+          score = 100 - play.play;
+        }
+      }
+      this.score.players[winner].win ++;
+      this.score.players[winner].score += score;
+      this.lastTurn = {
+        play:JSON.stringify(JSON.parse(this.play)),
+        winner:winner,
+        score:score
+      }
+      this.play = [];
+      this.played = 0;
+    }
+    return true;
+  }
+
+  gameStatForPlayer(player){
+    var gameStat = {};
+
+    gameStat.score = this.score;
+    gameStat.lastTurn = this.lastTurn;
+    gameStat.played = this.played;
+    var selfPlay = "";
+    for(play of this.play){
+      if(play.player == player){
+        selfPlay = play.play;
+      }
+    }
+    gameStat.selfPlay = selfPlay; 
+    
+    return gameStat;
+  }
+}
